d7f576460e03d26e3a8f57e386f91277cec0b912
Boffe123 <birkbonsaksen@gmail.com>
2026-03-21 17:16:14 +0100

    Added Database, and image to list of yarns

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
 from flask import Flask, redirect, request, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+import os
+from werkzeug.utils import secure_filename
+
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:bibi0086@localhost:5432/yarn_db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["UPLOAD_FOLDER"] = "static/uploads"
+
+db =SQLAlchemy(app)
+
 
 # Set up database 
 
 
-yarns = []
+class Yarn(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(50), nullable = False)
+    type = db.Column(db.String(20), nullable = False)
+    color = db.Column(db.String(20),nullable = False)
+    image = db.Column(db.String(500), nullable = False)
+
+
 
 @app.route("/wishlist.html")
 def wishlist():
@@ -21,20 +39,32 @@ def yarn_storage():
         name = request.form.get("name")
         type = request.form.get("type")
         color = request.form.get("color")
-        image = request.form.get("image")
+
+
+        file = request.files.get("image")
+
+        filename = None
+
+        if file and filename != "":
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+            file.save(filepath) 
         
         if name and color and type:
-            yarn = {
-                "name": name,
-                "type": type,
-                "color": color,
-                "image": image
-            }
-            yarns.append(yarn)
+            new_yarn = Yarn(
+                name = name,
+                type = type,
+                color = color,
+                image = filename
+            )
+            db.session.add(new_yarn)
+            db.session.commit()
         return redirect(url_for("yarn_storage"))
-    
-    reversed_yarns = list(reversed(yarns))
-    return render_template("yarn_storage.html", yarns=reversed_yarns)
-if __name__ == "__main__":  
+    yarns= Yarn.query.order_by(Yarn.id.desc()).all()
+   
+    return render_template("yarn_storage.html", yarns=yarns)
+if __name__ == "__main__": 
+    with app.app_context():
+        db.create_all() 
     app.run(debug=True)
 
